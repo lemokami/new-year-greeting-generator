6baabd0c05db58f1f699620e22ee5f93f8978893
lemokami <jesswinw@gmail.com>
2021-12-24 23:40:12 +0530

    feat: image and quotes for greeting and bg

---
+++ assets/quotes.ts
@@ -0,0 +1,8 @@
+export const quotes = [
+  `“One can never have enough socks," said Dumbledore. "Another Christmas has come and gone and I didn't get a single pair. People will insist on giving me books.”
+  `,
+  `“Then the Grinch thought of something he hadn't before! What if Christmas, he thought, doesn't come from a store. What if Christmas...perhaps...means a little bit more!”`,
+  `Christmas is the spirit of giving without a thought of getting. It is happiness because we see joy in people. It is forgetting self and finding time for others. It is discarding the meaningless and stressing the true values.`,
+  `Christmas isn't a season. It's a feeling.`,
+  `Don't let the past steal your present. This is the message of Christmas: We are never alone.`,
+];
