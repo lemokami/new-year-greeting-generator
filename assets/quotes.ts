b87460a99940e56e885d68b0d8a468f638530480
lemokami <jesswinw@gmail.com>
2021-12-26 01:29:17 +0530

    fix: quote had a random string from the template

--- assets/quotes.ts
+++ assets/quotes.ts
@@ -8,7 +8,7 @@ export const quotes = [
   'No one can go back in time to change what has happened, so work on your present to make yourself a wonderful future.',
   'You’re supposed to let go of the past and start off new. You’re supposed to forgive all those who have hurt you and be open to new relationships, with open arms. That is why it is called the ‘New’ Year. May you have a Happy New Year.',
   'End each year with a few good lessons and start the new one by showing that you have learnt the lessons of the past well.',
-  'The New Year has brought another chance for us to set things right and to open up a new chapter in our lives. Unlike what most people think it is never too late to be what you wish to be.new years card with religious message.',
+  'The New Year has brought another chance for us to set things right and to open up a new chapter in our lives. Unlike what most people think it is never too late to be what you wish to be.',
   'Failure doesn’t influence your inner resilience, and failing simply means that you’ve discovered another false way to move your life forward. Profit from it.',
   'At precisely the moment you feel like giving up, victory is always within reach. Remember this moving into the New Year.We will open the book. Its pages are blank. We are going to put words on them ourselves. The book is called Opportunity, and its first chapter is New Year’s Day',
 ];
