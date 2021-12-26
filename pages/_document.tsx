df90d8d52b807f58aa2b289b2a8289bef6a35ab2
lemokami <jesswinw@gmail.com>
2021-12-26 23:59:40 +0530

    refactor: some changes with description

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,18 +4,15 @@ const Document = () => (
   <Html>
     <Head>
       {/*  Primary Meta Tags  */}
-      <meta name='title' content='Greetings Generator' />
+      <meta name='title' content='New year greetings Generator' />
       <meta
         name='description'
-        content='Create greetings, to your loved ones with ease'
+        content='Create new year greetings to your loved ones with ease'
       />
 
       {/*  Open Graph / Facebook  */}
       <meta property='og:type' content='website' />
-      <meta
-        property='og:url'
-        content='https://greetings-generator.vercel.app'
-      />
+      <meta property='og:url' content='https://newyear-gen.vercel.app/' />
 
       <meta
         property='og:image'
@@ -24,18 +21,15 @@ const Document = () => (
 
       {/*  Twitter  */}
       <meta property='twitter:card' content='summary_large_image' />
-      <meta
-        property='twitter:url'
-        content='https://greetings-generator.vercel.app'
-      />
-      <meta property='twitter:title' content='Greetings Generator' />
+      <meta property='twitter:url' content='https://newyear-gen.vercel.app/' />
+      <meta property='twitter:title' content='New year greetings Generator' />
       <meta
         property='twitter:description'
         content='Create greetings, to your loved ones with ease'
       />
       <meta
         property='twitter:image'
-        content='https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png'
+        content='https://res.cloudinary.com/lemodrive/image/upload/v1640504612/greetings-generator/seo-banner-image.png'
       />
     </Head>
     <body>
--- pages/greeting.tsx
+++ pages/greeting.tsx
@@ -22,14 +22,14 @@ const Greeting: FC<GreetingProps> = ({ quote, name, greeter }) => {
           name='description'
           content={`${
             name === '' ? '' : name + ','
-          } Christmas is a season of enjoying the simple things that make life beautiful. May you have great memories that will permanently touch your heart. I wish you so much joy during this season and all through to the New Year`}
+          } New Year is a season of enjoying the simple things that make life beautiful. May you have great memories that will permanently touch your heart. I wish you so much joy during this season and all through to the New Year`}
         />
         <meta property='og:title' content={`New Year Wishes from ${greeter}`} />
         <meta
           property='og:desc'
           content={`${
             name === '' ? '' : name + ','
-          } Christmas is a season of enjoying the simple things that make life beautiful. May you have great memories that will permanently touch your heart. I wish you so much joy during this season and all through to the New Year`}
+          } New Year is a season of enjoying the simple things that make life beautiful. May you have great memories that will permanently touch your heart. I wish you so much joy during this season and all through to the New Year`}
         />
       </Head>
       {/* end seo  */}
